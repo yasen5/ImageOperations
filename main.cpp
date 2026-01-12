3e67fdff4661c7b08c3297f0a7a17e3693f4650b
yasen5 <167659798+yasen5@users.noreply.github.com>
2026-01-11 16:24:45 -0800

    Make testing easier

--- main.cpp
+++ main.cpp
@@ -4,34 +4,32 @@
 using namespace edge_detection;
 using MatrixXfRM = Matrix<float, Dynamic, Dynamic, RowMajor>;
 
+cv::Mat get_kerneled(const cv::Mat &image, Kernel kernel_type) {
+  Matrix<uint8_t, Dynamic, Dynamic, RowMajor> kerneled =
+      CleanupEdges(ApplyKernel(image, kernel_type), 0.3, 0.4);
+  const cv::Mat cv_kerneled(kerneled.rows(), kerneled.cols(), CV_8UC1,
+                      kerneled.data());
+  return cv_kerneled.clone();
+}
+
+void resize(std::vector<cv::Mat> &img_list) {
+  for (cv::Mat &img : img_list) {
+    cv::resize(img, img, img_list[0].size());
+  }
+}
+
 int main() {
   const std::string img_name = "test_images/test_image.png";
   const cv::Mat image =
       cv::imread("/Users/yasen/CLionProjects/ImageGradients/" + img_name,
                  cv::IMREAD_COLOR);
 
-  // Apply kernels
-  Matrix<uint8_t, Dynamic, Dynamic, RowMajor> kerneled1 =
-      CleanupEdges(ApplyKernel(image, ROBERTS), 0.3, 0.7);
-  cv::Mat cv_kerneled1(kerneled1.rows(), kerneled1.cols(), CV_8UC1,
-                       kerneled1.data());
-
-  Matrix<uint8_t, Dynamic, Dynamic, RowMajor> kerneled2 =
-      CleanupEdges(ApplyKernel(image, SOBEL3x3), 0.3, 0.4);
-  cv::Mat cv_kerneled2(kerneled2.rows(), kerneled2.cols(), CV_8UC1,
-                       kerneled2.data());
-
-  Matrix<uint8_t, Dynamic, Dynamic, RowMajor> kerneled3 =
-      CleanupEdges(ApplyKernel(image, SOBEL5x5), 0.3, 0.7);
-  cv::Mat cv_kerneled3(kerneled3.rows(), kerneled3.cols(), CV_8UC1,
-                       kerneled3.data());
-
-  cv::resize(cv_kerneled2, cv_kerneled2, cv_kerneled1.size());
-  cv::resize(cv_kerneled3, cv_kerneled3, cv_kerneled1.size());
-
+  std::vector<cv::Mat> kerneled{get_kerneled(image, ROBERTS),
+                                get_kerneled(image, SOBEL3x3),
+                                get_kerneled(image, SOBEL5x5)};
+  resize(kerneled);
   cv::Mat combined;
-  cv::hconcat(std::vector<cv::Mat>{cv_kerneled1, cv_kerneled2, cv_kerneled3},
-              combined);
+  cv::hconcat(kerneled, combined);
   cv::imshow("All Kernels Side-by-Side", combined);
   cv::waitKey(0);
   cv::destroyAllWindows();
